447dac0d8acf9378e367c57fea8904378e9e0719
Lenz Lösche <lenzloesche@gmail.com>
2023-02-21 13:01:07 +0100

    add turn card

--- components/card/card.js
+++ components/card/card.js
@@ -31,6 +31,27 @@ export function createCharacterCard(character) {
   } else {
     cardItem.style.boxShadow = "#D5AE2B 10px 5px 5px ";
   }
+  console.log(character);
+  cardItem.addEventListener("click", () => {
+    cardItem.classList.add("card--back-side");
+    cardItem.classList.toggle("transform-card");
+    cardItem.innerHTML = `
+    <div class="card__image-container">  
+      
+    </div>
+    <div class="card__content">
+      <h2 class="card__title">${character.name}</h2>
+      <dl class="card__info">
+        <dt class="card__info-title">Gender</dt>
+        <dd class="card__info-description">${character.gender}</dd>
+        <dt class="card__info-title">Location</dt>
+        <dd class="card__info-description">${character.location.name}</dd>
+        <dt class="card__info-title">Species</dt>
+        <dd class="card__info-description">${character.species}</dd>
+      </dl>
+    </div>
+    `;
+  });
 
   return cardItem;
 }
